97997f999b7256ed06e6266d6df5d4a4940f65a2
tangys <393977464@qq.com>
2016-07-13 17:23:49 +0800

    修改ScriptOp中的书写错误

--- sdk/AntShares/Core/Scripts/ScriptOp.py
+++ sdk/AntShares/Core/Scripts/ScriptOp.py
@@ -10,16 +10,16 @@ class ScriptOp(object):
 
     # Constants
     OP_0 = 0x00 # An empty array of bytes is pushed onto the stack. (This is not a no-op: an item is added to the stack.)
-    OP_FALSE = OP_0,
+    OP_FALSE = OP_0
     OP_PUSHBYTES1 = 0x01 # 0x01-0x4B The next opcode bytes is data to be pushed onto the stack
-    OP_PUSHBYTES75 = 0x4B,
+    OP_PUSHBYTES75 = 0x4B
     OP_PUSHDATA1 = 0x4C # The next byte contains the number of bytes to be pushed onto the stack.
     OP_PUSHDATA2 = 0x4D # The next two bytes contain the number of bytes to be pushed onto the stack.
     OP_PUSHDATA4 = 0x4E # The next four bytes contain the number of bytes to be pushed onto the stack.
     OP_1NEGATE = 0x4F # The number -1 is pushed onto the stack.
     # OP_RESERVED = 0x50 # Transaction is invalid unless occuring in an unexecuted OP_IF branch
     OP_1 = 0x51 # The number 1 is pushed onto the stack.
-    OP_TRUE = OP_1,
+    OP_TRUE = OP_1
     OP_2 = 0x52 # The number 2 is pushed onto the stack.
     OP_3 = 0x53 # The number 3 is pushed onto the stack.
     OP_4 = 0x54 # The number 4 is pushed onto the stack.
@@ -132,22 +132,22 @@ class ScriptOp(object):
     #OP_EVAL = 0xB0 # see BIP12. Currently disabled.
 
     # Array
-    OP_ARRAYSIZE = 0xC0,
-    OP_PACK = 0xC1,
-    OP_UNPACK = 0xC2,
-    OP_DISTINCT = 0xC3,
-    OP_SORT = 0xC4,
-    OP_REVERSE = 0xC5,
-    OP_CONCAT = 0xC6,
-    OP_UNION = 0xC7,
-    OP_INTERSECT = 0xC8,
-    OP_EXCEPT = 0xC9,
-    OP_TAKE = 0xCA,
-    OP_SKIP = 0xCB,
-    OP_PICKITEM = 0xCC,
-    OP_ALL = 0xCD,
-    OP_ANY = 0xCE,
-    OP_SUM = 0xCF,
-    OP_AVERAGE = 0xD0,
-    OP_MAXITEM = 0xD1,
-    OP_MINITEM = 0xD2,
+    OP_ARRAYSIZE = 0xC0
+    OP_PACK = 0xC1
+    OP_UNPACK = 0xC2
+    OP_DISTINCT = 0xC3
+    OP_SORT = 0xC4
+    OP_REVERSE = 0xC5
+    OP_CONCAT = 0xC6
+    OP_UNION = 0xC7
+    OP_INTERSECT = 0xC8
+    OP_EXCEPT = 0xC9
+    OP_TAKE = 0xCA
+    OP_SKIP = 0xCB
+    OP_PICKITEM = 0xCC
+    OP_ALL = 0xCD
+    OP_ANY = 0xCE
+    OP_SUM = 0xCF
+    OP_AVERAGE = 0xD0
+    OP_MAXITEM = 0xD1
+    OP_MINITEM = 0xD2
