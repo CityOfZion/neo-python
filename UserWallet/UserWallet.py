fde809a0e79d34c24b5a3d1a4cac27d329d5fdf2
tangys <393977464@qq.com>
2016-09-08 17:32:00 +0800

    Modify the UserWallet

--- UserWallet/UserWallet.py
+++ UserWallet/UserWallet.py
@@ -31,7 +31,12 @@ from AntShares.Helper import *
 
 from AntShares.Wallets.Account import Account
 from AntShares.Wallets.Contract import Contract
-a = Account(Prikey)
+
+# Creat Account
+
+privateKey = '7d989d02dff495cc1bbc35e891c153b98781e015a20ce276b86afc7856f85efa'
+
+a = Account(privateKey)
 print a.privateKey
 print a.publicKey
 print a.scriptHash
@@ -41,29 +46,21 @@ c = Contract()
 c.createSignatureContract(a.publicKey)
 print c.redeemScript
 
-
 # Create Outputs
 Outputs = [TransactionOutput(
             AssetId='f252a09a24591e8da31deec970871cc7678cb55023db049551e91f7bac28e27b',
             Value='19800.0',
-            ScriptHash='9c17b4ee1441676e36d77a141dd77869d271381d')]
+            ScriptHash=a.scriptHash)]
 
-# outputs = [{'Asset': u'AntCoin',
-#             'Value': u'100',
-#             'Scripthash': u'9c17b4ee1441676e36d77a141dd77869d271381d'}]
-#
-# inputs, coins, outputs = selectInputs(getInputs(), outputs)
-#
-# Inputs = [TransactionInput(
-#             prevHash=inputs[0][0].split('-')[0],
-#             prevIndex=inputs[0][0].split('-')[1])]
+# Create Inputs
 
 Inputs = [TransactionInput(
             prevHash='d9c8be3046cbc22799887219a469c6d89bbc95e45aebdf1252e40cc3d670f81d',
             prevIndex=0)]
 
-Issuer = '030fe41d11cc34a667cf1322ddc26ea4a8acad3b8eefa6f6c3f49c7673e4b33e4b'
-Admin = '9c17b4ee1441676e36d77a141dd77869d271381d'
+# Make RegisterTransaction
+Issuer = a.publicKey
+Admin = a.scriptHash
 tx = RegisterTransaction(Inputs, Outputs, 0x60, '测试', -0.00000001, Issuer, Admin)
 
 stream = MemoryStream()
@@ -72,17 +69,18 @@ tx.serializeUnsigned(writer)
 reg_tx = stream.toArray()
 
 print repr(reg_tx)
-print tx.getScriptHashesForVerifying()
-print big_or_little(sha256(binascii.unhexlify(reg_tx)))
+print tx.getScriptHashesForVerifying()  # TXID # Bug
 
-Prikey = '7d989d02dff495cc1bbc35e891c153b98781e015a20ce276b86afc7856f85efa'
-Redeem_script = '21030fe41d11cc34a667cf1322ddc26ea4a8acad3b8eefa6f6c3f49c7673e4b33e4bac'
+Redeem_script = c.redeemScript
+
+# Signature
 
 from ecdsa import SigningKey, NIST256p
-sk = SigningKey.from_string(binascii.unhexlify(Prikey), curve=NIST256p, hashfunc=hashlib.sha256)
+sk = SigningKey.from_string(binascii.unhexlify(a.privateKey), curve=NIST256p, hashfunc=hashlib.sha256)
 signature = binascii.hexlify(sk.sign(binascii.unhexlify(reg_tx),hashfunc=hashlib.sha256))
 regtx = reg_tx + '014140' + signature + '23' + Redeem_script
 
+# sendRawTransaction
 
 node = RemoteNode(url='http://47.90.66.157:20332')
 print node.getBestBlockhash()
