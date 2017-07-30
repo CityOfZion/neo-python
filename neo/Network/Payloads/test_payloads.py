d3ab36443d28f2004379d0d8ad7d1f7e8e156b49
Thomas Saunders <tasaunders@gmail.com>
2017-07-30 13:03:36 -0500

    adding payload tests back in

--- neo/Network/Payloads/test_payloads.py
+++ neo/Network/Payloads/test_payloads.py
@@ -23,3 +23,66 @@ class PayloadTestCase(unittest.TestCase):
 
 
         self.payload = VersionPayload(self.port, self.nonce, self.ua)
+
+    def test_version_create(self):
+
+        self.assertEqual(self.payload.Nonce, self.nonce)
+        self.assertEqual(self.payload.Port, self.port)
+        self.assertEqual(self.payload.UserAgent, self.ua)
+
+
+    def test_version_serialization(self):
+
+        serialized = binascii.unhexlify( Helper.ToArray(self.payload))
+
+        deserialized_version = AsSerializableWithType(serialized, 'neo.Network.Payloads.VersionPayload.VersionPayload')
+
+        v = deserialized_version
+
+        self.assertEqual(v.Nonce, self.nonce)
+        self.assertEqual(v.Port, self.port)
+        self.assertEqual(v.UserAgent, self.ua)
+        self.assertEqual(v.Timestamp, self.payload.Timestamp)
+        self.assertEqual(v.StartHeight, self.payload.StartHeight)
+        self.assertEqual(v.Version, self.payload.Version)
+        self.assertEqual(v.Services, self.payload.Services)
+        self.assertEqual(v.Relay, self.payload.Relay)
+
+
+    def test_message_serialization(self):
+
+        message = Message('version', payload=self.payload)
+
+        self.assertEqual(message.Command, 'version')
+
+        ms = MemoryStream()
+        writer = BinaryWriter(ms)
+
+        message.Serialize(writer)
+
+        result = binascii.unhexlify( ms.ToArray())
+
+        ms = MemoryStream(result)
+        reader = BinaryReader(ms)
+
+        deserialized_message = Message()
+        deserialized_message.Deserialize( reader )
+
+        dm = deserialized_message
+
+        self.assertEqual(dm.Command, 'version')
+
+        self.assertEqual(dm.Magic, Settings.MAGIC)
+
+        checksum = Message.GetChecksum(dm.Payload)
+
+        self.assertEqual(checksum, dm.Checksum)
+
+
+        deserialized_version = AsSerializableWithType(dm.Payload, 'neo.Network.Payloads.VersionPayload.VersionPayload')
+
+
+        self.assertEqual(deserialized_version.Port, self.port)
+        self.assertEqual(deserialized_version.UserAgent, self.ua)
+
+        self.assertEqual(deserialized_version.Timestamp, self.payload.Timestamp)
