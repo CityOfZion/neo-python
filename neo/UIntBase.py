643e7c3d34b1aafedd4ae80850a291d54c4c93f9
Chris Hager <chris@linuxuser.at>
2017-11-02 11:01:51 +0100

    UIntBase: __eq__ bugfix, tests and cleanup

--- neo/UIntBase.py
+++ neo/UIntBase.py
@@ -2,10 +2,7 @@ from neo.IO.Mixins import SerializableMixin
 import binascii
 
 class UIntBase(SerializableMixin):
-
-
     Data = bytearray()
-
     __hash = None
 
     def __init__(self, num_bytes, data=None):
@@ -26,14 +23,13 @@ class UIntBase(SerializableMixin):
                 raise Exception("Invalid format")
 
         self.__hash = self.GetHashCode()
+
     @property
     def Size(self):
         return len(self.Data)
 
     def GetHashCode(self):
-
         return int.from_bytes(self.Data[:4], 'little')
-
 
     def Serialize(self, writer):
         writer.WriteBytes(self.Data)
@@ -44,7 +40,6 @@ class UIntBase(SerializableMixin):
 
     def ToArray(self):
         return self.Data
-
 
     def ToString(self):
         db = bytearray(self.Data)
@@ -57,11 +52,11 @@ class UIntBase(SerializableMixin):
     def ToBytes(self):
         return bytes(self.ToString(), encoding='utf-8')
 
-
     def __eq__(self, other):
         if other is None:
             return False
-        if( type(self) != type(self)):
+
+        if type(other) != type(self):
             return False
 
         if other is self:
@@ -69,7 +64,6 @@ class UIntBase(SerializableMixin):
 
         if self.Data == other.Data:
             return True
-
 
     def __hash__(self):
         return self.__hash
