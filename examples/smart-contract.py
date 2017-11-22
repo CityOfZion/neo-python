3b4441c53dffa93b8bb11d4a76dfda7e4932b981
Chris Hager <chris@linuxuser.at>
2017-11-22 03:52:14 +0100

    setup.py + refactorings (contrib.smartcontract -> neo.contrib.smartcontract) (#97)
    * setup.py + refactorings (contrib.smartcontract ->
    neo.contrib.smartcontract)
    
    * Added setup.py and docs for `pip install -e .`
    * Moved contrib.smartcontract to neo.contrib.smartcontract
    * Renamed /contrib to /scripts
    * Added /examples with improved node.py (previously in /) and a smart contract
    example
    
    * Fixed setup.py

---
+++ examples/smart-contract.py
@@ -0,0 +1,81 @@
+"""
+Example of running a NEO node and receiving notifications when events
+of a specific smart contract happen.
+
+Events include Runtime.Notify, Runtime.Log, Storage.*, Execution.Success
+and several more.
+
+More documentation is coming soon.
+"""
+import threading
+from time import sleep
+
+from logzero import logger
+from twisted.internet import reactor, task
+
+from neo.contrib.smartcontract import SmartContract
+from neo.Network.NodeLeader import NodeLeader
+from neo.Core.Blockchain import Blockchain
+from neo.Implementations.Blockchains.LevelDB.LevelDBBlockchain import LevelDBBlockchain
+from neo.Settings import settings
+
+
+# If you want the log messages to also be saved in a logfile, enable the
+# next line. This configures a logfile with max 10 MB and 3 rotations:
+# settings.set_logfile("/tmp/logfile.log", max_bytes=1e7, backup_count=3)
+
+# Setup the smart contract instance
+smart_contract = SmartContract("6537b4bd100e514119e3a7ab49d520d20ef2c2a4")
+
+
+# Register an event handler for Runtime.Notify events of the smart contract.
+@smart_contract.on_notify
+def sc_notify(event):
+    print("SmartContract Runtime.Notify event:", event)
+
+    # Make sure that the event payload list has at least one element.
+    if not len(event.event_payload):
+        return
+
+    # The event payload list has at least one element. As developer of the smart contract
+    # you should know what data-type is in the bytes, and how to decode it. In this example,
+    # it's just a string, so we decode it with utf-8:
+    print("- payload part 1:", event.event_payload[0].decode("utf-8"))
+
+
+def custom_background_code():
+    """ Custom code run in a background thread. Prints the current block height.
+
+    This function is run in a daemonized thread, which means it can be instantly killed at any
+    moment, whenever the main thread quits. If you need more safety, don't use a  daemonized
+    thread and handle exiting this thread in another way (eg. with signals and events).
+    """
+    while True:
+        logger.info("Block %s / %s", str(Blockchain.Default().Height), str(Blockchain.Default().HeaderHeight))
+        sleep(15)
+
+
+def main():
+    # Setup the blockchain
+    blockchain = LevelDBBlockchain(settings.LEVELDB_PATH)
+    Blockchain.RegisterBlockchain(blockchain)
+    dbloop = task.LoopingCall(Blockchain.Default().PersistBlocks)
+    dbloop.start(.1)
+    NodeLeader.Instance().Start()
+
+    # Disable smart contract events for external smart contracts
+    settings.set_log_smart_contract_events(False)
+
+    # Start a thread with custom code
+    d = threading.Thread(target=custom_background_code)
+    d.setDaemon(True)  # daemonizing the thread will kill it when the main thread is quit
+    d.start()
+
+    # Run all the things (blocking call)
+    logger.info("Everything setup and running. Waiting for events...")
+    reactor.run()
+    logger.info("Shutting down.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+# get the requirements from requirements.txt
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='neo-python',
+    version='0.4.0',
+    description="Python Node and SDK for the NEO blockchain",
+    long_description=readme,
+    author="Thomas Saunders",
+    author_email='tom@cityofzion.io',
+    url='https://github.com/CityOfZion/neo-python',
+    packages=find_packages(include=['neo']),
+    include_package_data=True,
+    install_requires=reqs,
+    license="MIT license",
+    zip_safe=False,
+    keywords='neo, python, node',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
