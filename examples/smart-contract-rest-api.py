730cbf7588f75758cd4fc94bbdfd0df6cf79ca65
Chris Hager <chris@linuxuser.at>
2017-12-02 14:09:41 +0100

    neo.contrib.api.decorators + example with rest api

--- examples/smart-contract-rest-api.py
+++ examples/smart-contract-rest-api.py
@@ -1,34 +1,64 @@
 """
 Example of running a NEO node and receiving notifications when events
-of a specific smart contract happen.
+of a specific smart contract happen. This example also integrates
+a simple REST API.
 
-Events include Runtime.Notify, Runtime.Log, Storage.*, Execution.Success
-and several more. See the documentation here:
+Start this example like this:
+
+    API_TOKEN="test" python examples/smart-contract-rest-api.py
+
+Example API calls:
+
+    $ curl localhost:8080
+    $ curl -H "Authorization: Bearer test" localhost:8080/echo/hello123
+    $ curl -X POST -H "Authorization: Bearer test" -d '{ "msg": "foo" }' localhost:8080/echo-post
+
+Smart contract events include Runtime.Notify, Runtime.Log, Storage.*,
+Execution.Success and several more. See the documentation here:
 
 http://neo-python.readthedocs.io/en/latest/smartcontracts.html
+
+The REST API is using the Python package 'klein', which makes it
+possible to create HTTP routes and handlers with Twisted in a
+similar style to Flask. Klein is an official Twisted repository:
+https://github.com/twisted/klein
 """
+import os
 import threading
+import json
 from time import sleep
 
 from logzero import logger
-from twisted.internet import reactor, task
+from twisted.internet import reactor, task, endpoints
+from twisted.web.server import Request, Site
+from klein import Klein, resource
 
-from neo.contrib.smartcontract import SmartContract
 from neo.Network.NodeLeader import NodeLeader
 from neo.Core.Blockchain import Blockchain
 from neo.Implementations.Blockchains.LevelDB.LevelDBBlockchain import LevelDBBlockchain
 from neo.Settings import settings
 
+from neo.contrib.smartcontract import SmartContract
+from neo.contrib.api.decorators import json_response, authenticated, catch_exceptions
 
-# If you want the log messages to also be saved in a logfile, enable the
-# next line. This configures a logfile with max 10 MB and 3 rotations:
-# settings.set_logfile("/tmp/logfile.log", max_bytes=1e7, backup_count=3)
+SMART_CONTRACT_HASH = "6537b4bd100e514119e3a7ab49d520d20ef2c2a4"
+LOGFILE = None
+API_PORT = os.getenv("API_PORT", 8080)
+
+# If LOGFILE is set, file logging will be setup with max 10 MB per file and 3 rotations:
+if LOGFILE:
+    settings.set_logfile(LOGFILE, max_bytes=1e7, backup_count=3)
 
 # Setup the smart contract instance
-smart_contract = SmartContract("6537b4bd100e514119e3a7ab49d520d20ef2c2a4")
+smart_contract = SmartContract(SMART_CONTRACT_HASH)
+
+# Setup the klein instance
+app = Klein()
 
 
-# Register an event handler for Runtime.Notify events of the smart contract.
+#
+# Smart contract event handler for Runtime.Notify events
+#
 @smart_contract.on_notify
 def sc_notify(event):
     print("SmartContract Runtime.Notify event:", event)
@@ -43,6 +73,9 @@ def sc_notify(event):
     print("- payload part 1:", event.event_payload[0].decode("utf-8"))
 
 
+#
+# Custom code that runs in the background
+#
 def custom_background_code():
     """ Custom code run in a background thread. Prints the current block height.
 
@@ -55,6 +88,36 @@ def custom_background_code():
         sleep(15)
 
 
+#
+# REST API Routes
+#
+@app.route('/')
+def home(request):
+    return "Hello world"
+
+
+@app.route('/echo/<msg>')
+@catch_exceptions
+@authenticated
+@json_response
+def echo_msg(request, msg):
+    return {
+        "echo": msg
+    }
+
+@app.route('/echo-post', methods=['POST'])
+@catch_exceptions
+@authenticated
+@json_response
+def echo_post(request):
+    body = json.loads(request.content.read().decode("utf-8"))
+    return {
+        "post-body": body
+    }
+
+#
+# Main method which starts everything up
+#
 def main():
     # Setup the blockchain
     blockchain = LevelDBBlockchain(settings.LEVELDB_PATH)
@@ -70,6 +133,11 @@ def main():
     d = threading.Thread(target=custom_background_code)
     d.setDaemon(True)  # daemonizing the thread will kill it when the main thread is quit
     d.start()
+
+    # Hook up Klein API to Twisted reactor
+    endpoint_description = "tcp:port=%s:interface=localhost" % API_PORT
+    endpoint = endpoints.serverFromString(reactor, endpoint_description)
+    endpoint.listen(Site(app.resource()))
 
     # Run all the things (blocking call)
     logger.info("Everything setup and running. Waiting for events...")
---
+++ neo/contrib/api/decorators.py
@@ -0,0 +1,76 @@
+import os
+import json
+
+from functools import wraps
+from logzero import logger
+
+
+# Get the API token from an environment variable
+API_AUTH_TOKEN = os.getenv("API_TOKEN", None)
+if not API_AUTH_TOKEN:
+    raise Exception("No API_TOKEN environment variable found!")
+
+
+def json_response(func):
+    """
+    @json_response decorator adds response header for content type,
+    and json-dumps response object.
+    """
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        res = func(request, *args, **kwargs)
+        request.setHeader('Content-Type', 'application/json')
+        return json.dumps(res) if isinstance(res, dict) else res
+    return wrapper
+
+
+def authenticated(func):
+    """
+    @authenticated decorator, which makes sure the HTTP request has the correct access token
+
+    Header has to be in the format "Authorization: Bearer {token}"
+
+    If no valid header is part of the request's HTTP headers, this decorator automatically
+    returns the HTTP status code 403.
+    """
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        # Make sure Authorization header is present
+        if not request.requestHeaders.hasHeader("Authorization"):
+            request.setHeader('Content-Type', 'application/json')
+            request.setResponseCode(403)
+            return json.dumps({ "error": "No Authorization header found" })
+
+        # Make sure Authorization header is valid
+        user_auth_token = str(request.requestHeaders.getRawHeaders("Authorization")[0])
+        if user_auth_token != "Bearer %s" % API_AUTH_TOKEN:
+            request.setHeader('Content-Type', 'application/json')
+            request.setResponseCode(403)
+            return json.dumps({ "error": "Invalid Authorization header" })
+
+        # If all good, proceed to request handler
+        return func(request, *args, **kwargs)
+    return wrapper
+
+
+def catch_exceptions(func):
+    """
+    @catch_exceptions decorator handles generic exceptions in the request handler.
+    All uncaught exceptions will be packaged into a nice JSON response, and returned
+    to the caller with status code 500.
+
+    This is especially useful for development, for production you might want to
+    disable the messages.
+    """
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        try:
+            res = func(request, *args, **kwargs)
+        except Exception as e:
+            logger.exception(e)
+            request.setResponseCode(500)
+            request.setHeader('Content-Type', 'application/json')
+            return json.dumps({ "error": str(e) })
+        return res
+    return wrapper
+
