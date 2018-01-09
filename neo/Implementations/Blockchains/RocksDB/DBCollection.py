a11cb9ba95f6871dc79a518f63da7cdfb093566d
xingfang0408 <fang.xing@gmail.com>
2018-01-09 10:20:58 -0500

    add support to rocksdb (#151)
    * add rocksdb
    
    * migrate rocksdb testcases
    
    * fixed pycodestyle neo reported errors and convert print to proper logging

---
+++ neo/Implementations/Blockchains/RocksDB/DBCollection.py
@@ -0,0 +1,170 @@
+import binascii
+import inspect
+from logzero import logger
+
+from neo.Implementations.Blockchains.RocksDB.DBPrefix import DBPrefix
+
+
+class DBCollection():
+
+    DB = None
+#    SN = None
+    Prefix = None
+
+    ClassRef = None
+
+    Collection = {}
+
+    Changed = []
+    Deleted = []
+
+    _built_keys = False
+
+    DebugStorage = False
+
+    def __init__(self, db, sn, prefix, class_ref):
+
+        self.DB = db
+
+        self.Prefix = prefix
+
+        self.ClassRef = class_ref
+
+        self.Collection = {}
+        self.Changed = []
+        self.Deleted = []
+
+    @property
+    def Keys(self):
+        if not self._built_keys:
+            self._BuildCollectionKeys()
+
+        return self.Collection.keys()
+
+    @property
+    def Current(self):
+        try:
+            ret = {}
+            for key, val in self.Collection.items():
+                if val is not None:
+                    ret[key] = val
+            return ret
+        except Exception as e:
+            logger.error("error getting items %s " % e)
+
+        return {}
+
+    def _BuildCollectionKeys(self):
+        for key in self.DB.iterator(prefix=self.Prefix, include_value=False):
+            key = key[1:]
+            if key not in self.Collection.keys():
+                self.Collection[key] = None
+
+    def Commit(self, wb, destroy=True):
+
+        for keyval in self.Changed:
+            item = self.Collection[keyval]
+            if item:
+                self.DB.put(self.Prefix + keyval, self.Collection[keyval].ToByteArray())
+        for keyval in self.Deleted:
+            self.DB.delete(self.Prefix + keyval)
+            self.Collection[keyval] = None
+        if destroy:
+            self.Destroy()
+        else:
+            self.Changed = []
+            self.Deleted = []
+
+    def GetAndChange(self, keyval, new_instance=None, debug_item=False):
+
+        item = self.TryGet(keyval)
+
+        if item is None:
+            if new_instance is None:
+                item = self.ClassRef()
+            else:
+                item = new_instance
+
+            self.Add(keyval, item)
+
+        self.MarkChanged(keyval)
+
+        return item
+
+    def GetOrAdd(self, keyval, new_instance):
+
+        item = new_instance
+
+        if keyval in self.Deleted:
+            self.Deleted.remove(keyval)
+
+        self.Add(keyval, item)
+
+        return item
+
+    def GetItemBy(self, keyval):
+        return self.GetAndChange(keyval)
+
+    def TryGet(self, keyval):
+
+        if keyval in self.Deleted:
+            return None
+
+        if keyval in self.Collection.keys():
+            item = self.Collection[keyval]
+            if item is None:
+                item = self._GetItem(keyval)
+            self.MarkChanged(keyval)
+            return item
+
+        # otherwise, chekc in the database
+        key = self.DB.get(self.Prefix + keyval)
+
+        # if the key is there, get the item
+        if key is not None:
+
+            self.MarkChanged(keyval)
+
+            item = self._GetItem(keyval)
+
+            return item
+
+        return None
+
+    def _GetItem(self, keyval):
+        if keyval in self.Deleted:
+            return None
+
+        try:
+            buffer = self.DB.get(self.Prefix + keyval)
+            if buffer:
+                item = self.ClassRef.DeserializeFromDB(binascii.unhexlify(buffer))
+                self.Collection[keyval] = item
+                return item
+            return None
+        except Exception as e:
+            logger.error("Could not deserialize item from key %s : %s" % (keyval, e))
+
+        return None
+
+    def Add(self, keyval, item):
+        self.Collection[keyval] = item
+        self.MarkChanged(keyval)
+
+    def Remove(self, keyval):
+        if keyval not in self.Deleted:
+            self.Deleted.append(keyval)
+
+    def MarkChanged(self, keyval):
+        if keyval not in self.Changed:
+            self.Changed.append(keyval)
+
+    def Destroy(self):
+        self.DB = None
+#        self.SN = None
+        self.Collection = None
+        self.ClassRef = None
+        self.Prefix = None
+        self.Deleted = None
+        self.Changed = None
+        logger = None
---
+++ neo/Implementations/Blockchains/RocksDB/test/test_initial_db.py
@@ -0,0 +1,63 @@
+from neo.Utils.NeoTestCase import NeoTestCase
+from neo.Implementations.Blockchains.RocksDB.RocksDBBlockchain import RocksDBBlockchain
+from neo.Core.Blockchain import Blockchain
+from neo.IO.Helper import Helper
+import shutil
+import binascii
+from neo.Settings import settings
+
+
+class RocksDBTest(NeoTestCase):
+
+    print("rocksdbtest")
+    ROCKSDB_TESTPATH = './UnitTestChain'
+
+    _blockchain = None
+
+    _genesis = None
+
+    block_one_raw = b'00000000ef1f8f66a16fba100ed760f4ac6aa5a0d0bb8f4a0e92705b106761ef181718b3d0765298ceb5f57de7d2b0dab00ed25be4134706ada2d90adb8b7e3aba323a8e1abd125901000000d11f7a289214bdaff3812db982f3b0089a21a278988efeec6a027b2501fd450140884037dd265cb5f5a54802f53c2c8593b31d5b8a9c0bad4c7e366b153d878989d168080ac36b930036a9eb966b48c70bb41792e698fa021116f27c09643563b840e83ab14404d964a91dbac45f5460e88ad57196b1779478e3475334af8c1b49cd9f0213257895c60b5b92a4800eb32d785cbb39ae1f022528943909fd37deba63403677848bf98cc9dbd8fbfd7f2e4f34471866ea82ca6bffbf0f778b6931483700c17829b4bd066eb04983d3aac0bd46b9c8d03a73a8e714d3119de93cd9522e314054d16853b22014190063f77d9edf6fbccefcf71fffd1234f688823b4e429ae5fa639d0a664c842fbdfcb4d6e21f39d81c23563b92cffa09696d93c95bc4893a6401a43071d00d3e854f7f1f321afa7d5301d36f2195dc1e2643463f34ae637d2b02ae0eb11d4256c507a4f8304cea6396a7fce640f50acb301c2f6336d27717e84f155210209e7fd41dfb5c2f8dc72eb30358ac100ea8c72da18847befe06eade68cebfcb9210327da12b5c40200e9f65569476bbff2218da4f32548ff43b6387ec1416a231ee821034ff5ceeac41acf22cd5ed2da17a6df4dd8358fcb2bfb1a43208ad0feaab2746b21026ce35b29147ad09e4afe4ec4a7319095f08198fa8babbe3c56e970b143528d2221038dddc06ce687677a53d54f096d2591ba2302068cf123c1f2d75c2dddc542557921039dafd8571a641058ccc832c5e2111ea39b09c0bde36050914384f7a48bce9bf92102d02b1873a0863cd042cc717da31cea0d7cf9db32b74d4c72c01b0011503e2e2257ae010000d11f7a2800000000'
+    block_one_hash = b'0012f8566567a9d7ddf25acb5cf98286c9703297de675d01ba73fbfe6bcb841c'
+
+    @classmethod
+    def setUpClass(self):
+        print("setup")
+        self._blockchain = RocksDBBlockchain(path=self.ROCKSDB_TESTPATH)
+        Blockchain.RegisterBlockchain(self._blockchain)
+        self._genesis = Blockchain.GenesisBlock()
+
+    @classmethod
+    def tearDownClass(self):
+        print("teardown")
+        self._blockchain.Dispose()
+        shutil.rmtree(self.ROCKSDB_TESTPATH)
+
+    def test__initial_state(self):
+        print("initial_state")
+        self.assertEqual(self._blockchain.CurrentBlockHash, self._genesis.Hash.ToBytes())
+
+        self.assertEqual(self._blockchain.CurrentHeaderHash, self._genesis.Header.Hash.ToBytes())
+
+        self.assertEqual(self._blockchain.CurrentHeaderHash, self._genesis.Header.Hash.ToBytes())
+
+        self.assertEqual(self._blockchain.HeaderHeight, 0)
+
+        self.assertEqual(self._blockchain.Height, 0)
+
+    def test_add_header(self):
+        print("add header")
+        hexdata = binascii.unhexlify(self.block_one_raw)
+
+        block_one = Helper.AsSerializableWithType(hexdata, 'neo.Core.Block.Block')
+
+        if settings.MAGIC == 1953787457:
+            self.assertEqual(self._blockchain.CurrentHeaderHash, b'b3181718ef6167105b70920e4a8fbbd0a0a56aacf460d70e10ba6fa1668f1fef')
+        else:
+            self.assertEqual(self._blockchain.CurrentHeaderHash, b'd42561e3d30e15be6400b6df2f328e02d2bf6354c41dce433bc57687c82144bf')
+
+        self.assertEqual(self._blockchain.HeaderHeight, 0)
+
+    def test_sys_block_fees(self):
+        print("sys block fees")
+        block_num = 14103
+        fee_should_be = 435
