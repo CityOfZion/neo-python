8c541d1c2b345d8acd0cb05b2f66828080c7b2cc
Michael Jakl <mj@to.hn>
2018-01-21 20:45:21 +0100

    Conform to the double-quoted strings style

--- neo/Prompt/test_input_parser.py
+++ neo/Prompt/test_input_parser.py
@@ -6,41 +6,41 @@ class TestInputParser(TestCase):
     input_parser = InputParser()
 
     def test_simple_whitespace_separation(self):
-        command, arguments = self.input_parser.parse_input('this is a simple test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['is', 'a', 'simple', 'test'])
+        command, arguments = self.input_parser.parse_input("this is a simple test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["is", "a", "simple", "test"])
 
     def test_keeping_double_quoted_strings_together(self):
-        command, arguments = self.input_parser.parse_input('this "is a simple" test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['"is a simple"', 'test'])
+        command, arguments = self.input_parser.parse_input("this \"is a simple\" test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["\"is a simple\"", "test"])
 
     def test_keeping_single_quoted_strings_together(self):
-        command, arguments = self.input_parser.parse_input('this \'is a simple\' test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['\'is a simple\'', 'test'])
+        command, arguments = self.input_parser.parse_input("this 'is a simple' test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["'is a simple'", "test"])
 
     def test_keeping_bracket_elements_together(self):
-        command, arguments = self.input_parser.parse_input('this [is a simple] test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['[is a simple]', 'test'])
+        command, arguments = self.input_parser.parse_input("this [is a simple] test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["[is a simple]", "test"])
 
     def test_keeping_brackets_and_strings_together(self):
-        command, arguments = self.input_parser.parse_input('this [is \'a simple\'] test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['[is \'a simple\']', 'test'])
+        command, arguments = self.input_parser.parse_input("this [is \"a simple\"] test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["[is \"a simple\"]", "test"])
 
     def test_unmatched_brackets(self):
-        command, arguments = self.input_parser.parse_input('this [is \'a simple\' test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['[is', '\'a simple\'', 'test'])
+        command, arguments = self.input_parser.parse_input("this [is \"a simple\" test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["[is", "\"a simple\"", "test"])
 
     def test_unmatched_single_quotes(self):
-        command, arguments = self.input_parser.parse_input('this is \'a simple test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['is', '\'a', 'simple', 'test'])
+        command, arguments = self.input_parser.parse_input("this is 'a simple test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["is", "'a", "simple", "test"])
 
     def test_unmatched_double_quotes(self):
-        command, arguments = self.input_parser.parse_input('this is "a simple test')
-        self.assertEqual(command, 'this')
-        self.assertEqual(arguments, ['is', '"a', 'simple', 'test'])
+        command, arguments = self.input_parser.parse_input("this is \"a simple test")
+        self.assertEqual(command, "this")
+        self.assertEqual(arguments, ["is", "\"a", "simple", "test"])
