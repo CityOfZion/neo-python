838d97f69a8dbb86a23c4de1a56916e1fa0ad05f
Michael Jakl <mj@to.hn>
2018-01-21 14:41:55 +0100

    Implement a more robust user-input parser
    Detects brackets and quoted strings and keeps them together (even
    with space between them). After seeing many people struggle with
    it, this should save a lot of time.

---
+++ neo/Prompt/test_input_parser.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from neo.Prompt.InputParser import InputParser
+
+
+class TestInputParser(TestCase):
+    input_parser = InputParser()
+
+    def test_simple_whitespace_separation(self):
+        command, arguments = self.input_parser.parse_input('this is a simple test')
+        self.assertEqual(command, 'this')
+        self.assertEqual(arguments, ['is', 'a', 'simple', 'test'])
+
+    def test_keeping_double_quoted_strings_together(self):
+        command, arguments = self.input_parser.parse_input('this "is a simple" test')
+        self.assertEqual(command, 'this')
+        self.assertEqual(arguments, ['"is a simple"', 'test'])
+
+    def test_keeping_single_quoted_strings_together(self):
+        command, arguments = self.input_parser.parse_input('this \'is a simple\' test')
+        self.assertEqual(command, 'this')
+        self.assertEqual(arguments, ['\'is a simple\'', 'test'])
+
+    def test_keeping_bracket_elements_together(self):
+        command, arguments = self.input_parser.parse_input('this [is a simple] test')
+        self.assertEqual(command, 'this')
+        self.assertEqual(arguments, ['[is a simple]', 'test'])
+
+    def test_keeping_brackets_and_strings_together(self):
+        command, arguments = self.input_parser.parse_input('this [is \'a simple\'] test')
+        self.assertEqual(command, 'this')
+        self.assertEqual(arguments, ['[is \'a simple\']', 'test'])
