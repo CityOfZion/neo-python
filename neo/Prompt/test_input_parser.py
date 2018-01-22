f904f5bb0463c45b1a6da8dd1e2b28b794df7055
Michael Jakl <mj@to.hn>
2018-01-22 20:03:24 +0100

    Add test showing Python bytearrays are still parsed correctly

--- neo/Prompt/test_input_parser.py
+++ neo/Prompt/test_input_parser.py
@@ -44,3 +44,8 @@ class TestInputParser(TestCase):
         command, arguments = self.input_parser.parse_input("this is \"a simple test")
         self.assertEqual(command, "this")
         self.assertEqual(arguments, ["is", "\"a", "simple", "test"])
+
+    def test_python_bytearrays(self):
+        command, arguments = self.input_parser.parse_input("testinvoke f8d448b227991cf07cb96a6f9c0322437f1599b9 transfer [bytearray(b'S\xefB\xc8\xdf!^\xbeZ|z\xe8\x01\xcb\xc3\xac/\xacI)'), bytearray(b'\xaf\x12\xa8h{\x14\x94\x8b\xc4\xa0\x08\x12\x8aU\nci[\xc1\xa5'), 1000]")
+        self.assertEqual(command, "testinvoke")
+        self.assertEqual(arguments, ["f8d448b227991cf07cb96a6f9c0322437f1599b9", "transfer", "[bytearray(b'S\xefB\xc8\xdf!^\xbeZ|z\xe8\x01\xcb\xc3\xac/\xacI)'), bytearray(b'\xaf\x12\xa8h{\x14\x94\x8b\xc4\xa0\x08\x12\x8aU\nci[\xc1\xa5'), 1000]"])
