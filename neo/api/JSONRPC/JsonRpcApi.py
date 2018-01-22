fff6fb08c9dfda2929b8d36b24a2696761d4fec8
Chris Hager <chris@linuxuser.at>
2018-01-22 21:21:12 +0100

    JSON-RPC Start proper method and error handling

--- neo/api/JSONRPC/JsonRpcApi.py
+++ neo/api/JSONRPC/JsonRpcApi.py
@@ -8,31 +8,35 @@ See also:
 """
 import json
 from json.decoder import JSONDecodeError
-from functools import wraps
 
 from klein import Klein
 from neo.Implementations.Notifications.LevelDB.NotificationDB import NotificationDB
 from logzero import logger
 from neo.Core.Blockchain import Blockchain
+from neo.api.utils import json_response
 
 
-class Errors:
+class JsonRpcErrors:
     PARSE_ERROR = {"code": -32700, "message": "Parse error"}
     INVALID_REQUEST = {"code": -32600, "message": "Invalid Request"}
     METHOD_NOT_FOUND = {"code": -32601, "message": "Method not found"}
     INVALID_PARAMS = {"code": -32602, "message": "Invalid params"}
     INTERNAL_ERROR = {"code": -32603, "message": "Internal error"}
-    INVALID_PARAMS = {"code": -32000, "message": "Server error"}
+    SERVER_ERROR = {"code": -32000, "message": "Server error"}
 
 
-def json_response(func):
-    """ @json_response decorator adds header and dumps response object """
-    @wraps(func)
-    def wrapper(self, request, *args, **kwargs):
-        res = func(self, request, *args, **kwargs)
-        request.setHeader('Content-Type', 'application/json')
-        return json.dumps(res) if isinstance(res, dict) else res
-    return wrapper
+class JsonRpcError(Exception):
+    message = None
+    json_rpc_error = None
+
+    def __init__(self, message, json_rpc_error, json_rpc_error_code=None):
+        super(JsonRpcError, self).__init__(message)
+        self.message = message
+        self.error_data = {"error_message": message}
+
+        self.json_rpc_error = json_rpc_error
+        if json_rpc_error_code:
+            self.json_rpc_error["code"] = json_rpc_error_code
 
 
 class JsonRpcApi(object):
@@ -43,25 +47,51 @@ class JsonRpcApi(object):
         self.notif = NotificationDB.instance()
 
     #
-    # JSON-RPC Route
+    # JSON-RPC API Route
     #
     @app.route('/')
     @json_response
     def home(self, request):
         # {"jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": []}
+        body = None
         try:
             body = json.loads(request.content.read().decode("utf-8"))
+            if "jsonrpc" not in body or body["jsonrpc"] != "2.0":
+                raise JsonRpcError("Invalid value for 'jsonrpc'", JsonRpcErrors.INVALID_REQUEST)
+
+            if "id" not in body:
+                raise JsonRpcError("Field 'id' is missing", JsonRpcErrors.INVALID_REQUEST)
+
+            if "method" not in body:
+                raise JsonRpcError("Field 'method' is missing", JsonRpcErrors.INVALID_REQUEST)
+
+            params = body["params"] if "params" in body else None
+            result = self.json_rpc_method_handler(body["method"], params)
+            return self.get_response(body["id"], result)
 
         except JSONDecodeError as e:
-            return self.get_error_payload(None, Errors.PARSE_ERROR, data={"exception": str(e)})
+            request_id = body["id"] if body and "id" in body else None
+            return self.get_error_payload(request_id, JsonRpcErrors.PARSE_ERROR, data={"error_message": str(e)})
+
+        except JsonRpcError as e:
+            request_id = body["id"] if body and "id" in body else None
+            return self.get_error_payload(request_id, e.json_rpc_error, data=e.error_data)
 
         except Exception as e:
-            return self.get_error_payload(None, Errors.INTERNAL_ERROR, data={"exception": str(e)})
+            request_id = body["id"] if body and "id" in body else None
+            return self.get_error_payload(request_id, JsonRpcErrors.INTERNAL_ERROR, data={"error_message": str(e)})
 
-    def get_error_payload(self, id, error, data=None, code=None):
+    def get_response(self, request_id, result):
+        return {
+            "jsonrpc": "2.0",
+            "id": request_id,
+            "result": result
+        }
+
+    def get_error_payload(self, request_id, error, data=None, code=None):
         error_payload = {
             "jsonrpc": "2.0",
-            "id": id,
+            "id": request_id,
             "error": error
         }
 
@@ -72,3 +102,9 @@ class JsonRpcApi(object):
             error_payload["data"] = data
 
         return error_payload
+
+    def json_rpc_method_handler(self, method, params):
+        # print("method", method, params)
+        if method == "getblockcount":
+            return str(Blockchain.Default().HeaderHeight)
+        raise JsonRpcError("Method '%s' not found" % method, JsonRpcErrors.METHOD_NOT_FOUND)
--- neo/api/JSONRPC/test_json_rpc_api.py
+++ neo/api/JSONRPC/test_json_rpc_api.py
@@ -21,50 +21,43 @@ def mock_request(body):
 
 
 class JsonRpcApiTestCase(TestCase):
-    # FIXTURE_REMOTE_LOC = 'https://s3.us-east-2.amazonaws.com/cityofzion/fixtures/notif_fixture.tar.gz'
-    # FIXTURE_FILENAME = './Chains/notif_fixture.tar.gz'
-    # NOTIFICATION_DB_NAME = 'fixtures/test_notifications'
-
-    # contract_hash = UInt160(data=bytearray(b'\x11\xc4\xd1\xf4\xfb\xa6\x19\xf2b\x88p\xd3n:\x97s\xe8tp['))
-    # event_tx = '042e4168cb2d563714d3f35ff76b7efc6c7d428360c97b6b45a18b5b1a4faa40'
-
-    # addr_to = 'AHbmRX5sL8oxp4dJZRNg5crCGUGxuMUyRB'
-    # addr_from = 'AcFnRrVC5emrTEkuFuRPufcuTb6KsAJ3vR'
+    FIXTURE_REMOTE_LOC = 'https://s3.us-east-2.amazonaws.com/cityofzion/fixtures/notif_fixture.tar.gz'
+    FIXTURE_FILENAME = './Chains/notif_fixture.tar.gz'
+    NOTIFICATION_DB_NAME = 'fixtures/test_notifications'
 
     app = None  # type:JsonRpcApi
 
-    # @classmethod
-    # def setUpClass(cls):
+    @classmethod
+    def setUpClass(cls):
+        if not os.path.exists(cls.FIXTURE_FILENAME):
+            logzero.logger.info(
+                "downloading fixture notification database from %s. this may take a while" % cls.FIXTURE_REMOTE_LOC)
 
-    #     if not os.path.exists(cls.FIXTURE_FILENAME):
-    #         logzero.logger.info(
-    #             "downloading fixture notification database from %s. this may take a while" % cls.FIXTURE_REMOTE_LOC)
+            response = requests.get(cls.FIXTURE_REMOTE_LOC, stream=True)
 
-    #         response = requests.get(cls.FIXTURE_REMOTE_LOC, stream=True)
+            response.raise_for_status()
+            with open(cls.FIXTURE_FILENAME, 'wb+') as handle:
+                for block in response.iter_content(1024):
+                    handle.write(block)
 
-    #         response.raise_for_status()
-    #         with open(cls.FIXTURE_FILENAME, 'wb+') as handle:
-    #             for block in response.iter_content(1024):
-    #                 handle.write(block)
+        try:
+            tar = tarfile.open(cls.FIXTURE_FILENAME)
+            tar.extractall()
+            tar.close()
+        except Exception as e:
+            raise Exception("Could not extract tar file - %s. You may want need to remove the fixtures file %s manually to fix this." % (e, cls.FIXTURE_FILENAME))
 
-    #     try:
-    #         tar = tarfile.open(cls.FIXTURE_FILENAME)
-    #         tar.extractall()
-    #         tar.close()
-    #     except Exception as e:
-    #         raise Exception("Could not extract tar file - %s. You may want need to remove the fixtures file %s manually to fix this." % (e, cls.FIXTURE_FILENAME))
+        if not os.path.exists(cls.NOTIFICATION_DB_NAME):
+            raise Exception("Error downloading fixtures")
 
-    #     if not os.path.exists(cls.NOTIFICATION_DB_NAME):
-    #         raise Exception("Error downloading fixtures")
+        settings.NOTIFICATION_DB_PATH = cls.NOTIFICATION_DB_NAME
+        ndb = NotificationDB.instance()
+        ndb.start()
 
-    #     settings.NOTIFICATION_DB_PATH = cls.NOTIFICATION_DB_NAME
-    #     ndb = NotificationDB.instance()
-    #     ndb.start()
-
-    # @classmethod
-    # def tearDownClass(cls):
-    #     NotificationDB.instance().close()
-    #     shutil.rmtree(cls.NOTIFICATION_DB_NAME)
+    @classmethod
+    def tearDownClass(cls):
+        NotificationDB.instance().close()
+        shutil.rmtree(cls.NOTIFICATION_DB_NAME)
 
     def setUp(self):
         self.app = JsonRpcApi()
@@ -86,65 +79,50 @@ class JsonRpcApiTestCase(TestCase):
         res = json.loads(self.app.home(mock_req))
         self.assertEqual(res["error"]["code"], -32700)
 
-    # def test_4_by_block(self):
-    #     mock_req = requestMock(path=b'/block/206939')
-    #     res = self.app.get_by_block(mock_req, 206939)
-    #     jsn = json.loads(res)
-    #     self.assertEqual(jsn['total'], 1)
-    #     results = jsn['results']
-    #     self.assertEqual(len(results), 1)
+        mock_req = mock_request(json.dumps({"some": "stuff"}).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+        self.assertEqual(res["error"]["code"], -32600)
 
-    # def test_5_block_no_results(self):
-    #     mock_req = requestMock(path=b'/block/206')
-    #     res = self.app.get_by_block(mock_req, 206)
-    #     jsn = json.loads(res)
-    #     self.assertEqual(jsn['total'], 0)
-    #     results = jsn['results']
-    #     self.assertIsInstance(results, list)
-    #     self.assertEqual(len(results), 0)
+    def _gen_rpc_req(self, method, params=None, request_id="2"):
+        ret = {
+            "jsonrpc": "2.0",
+            "id": request_id,
+            "method": method
+        }
+        if params:
+            ret["params"] = params
+        return ret
 
-    # def test_6_block_num_too_big(self):
-    #     mock_req = requestMock(path=b'/block/2060200054055066')
-    #     res = self.app.get_by_block(mock_req, 2060200054055066)
-    #     jsn = json.loads(res)
-    #     self.assertEqual(jsn['total'], 0)
-    #     results = jsn['results']
-    #     self.assertIsInstance(results, list)
-    #     self.assertIn('Could not get notifications', jsn['message'])
+    def test_missing_fields(self):
+        req = self._gen_rpc_req("foo")
+        del req["jsonrpc"]
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+        self.assertEqual(res["error"]["code"], -32600)
 
-    # def test_7_by_addr(self):
-    #     mock_req = requestMock(path=b'/addr/AcFnRrVC5emrTEkuFuRPufcuTb6KsAJ3vR')
-    #     res = self.app.get_by_addr(mock_req, 'AcFnRrVC5emrTEkuFuRPufcuTb6KsAJ3vR')
-    #     jsn = json.loads(res)
-    #     self.assertEqual(jsn['total'], 3)
-    #     results = jsn['results']
-    #     self.assertEqual(len(results), 3)
+        req = self._gen_rpc_req("foo")
+        del req["id"]
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+        self.assertEqual(res["error"]["code"], -32600)
 
-    # def test_8_bad_addr(self):
-    #     mock_req = requestMock(path=b'/addr/AcFnRrVC5emrTEkuFuRPufcuTb6KsAJ3v')
-    #     res = self.app.get_by_addr(mock_req, 'AcFnRrVC5emrTEkuFuRPufcuTb6KsAJ3v')
-    #     jsn = json.loads(res)
-    #     self.assertEqual(jsn['total'], 0)
-    #     results = jsn['results']
-    #     self.assertIsInstance(results, list)
-    #     self.assertIn('Could not get notifications', jsn['message'])
+        req = self._gen_rpc_req("foo")
+        del req["method"]
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+        self.assertEqual(res["error"]["code"], -32600)
 
-    # this following test doesn't work because we'd have to load in the blockchain that matches these notifications
+    def test_invalid_method(self):
+        req = self._gen_rpc_req("invalid", request_id="42")
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+        self.assertEqual(res["id"], "42")
+        self.assertEqual(res["error"]["code"], -32601)
 
-    # def test_9_by_tx(self):
-    #     mock_req = requestMock(path=b'/tx/042e4168cb2d563714d3f35ff76b7efc6c7d428360c97b6b45a18b5b1a4faa40')
-    #     res = self.app.get_by_tx(mock_req,b'042e4168cb2d563714d3f35ff76b7efc6c7d428360c97b6b45a18b5b1a4faa40')
-    #     jsn = json.loads(res)
-    #     print("JSON %s " % jsn)
-    #     self.assertEqual(jsn['total'],1)
-    #     results = jsn['results']
-    #     self.assertEqual(len(results), 1)
-
-    # def test_9_by_bad_tx(self):
-    #     mock_req = requestMock(path=b'/tx/2e4168cb2d563714d3f35ff76b7efc6c7d428360c97b6b45a18b5b1a4faa40')
-    #     res = self.app.get_by_tx(mock_req, b'2e4168cb2d563714d3f35ff76b7efc6c7d428360c97b6b45a18b5b1a4faa40')
-    #     jsn = json.loads(res)
-    #     self.assertEqual(jsn['total'], 0)
-    #     results = jsn['results']
-    #     self.assertIsInstance(results, list)
-    #     self.assertIn('Could not get tx with hash', jsn['message'])
+    def test_getblockcount(self):
+        # TODO: currently not returning a correct header height
+        ndb = NotificationDB.instance()
+        req = self._gen_rpc_req("getblockcount")
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+        self.assertEqual(res["result"], 123)
