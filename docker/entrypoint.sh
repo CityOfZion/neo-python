a97244e247a4aa994f149c65d07d7671030d8b3d
jberg <jorrit.vandenberg@tno.nl>
2018-03-12 20:59:29 +0100

    Fixed entrypoint.sh script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 PROMPT_OPTS=()
 BOOTSTRAP_OPTS=()
-BOOTSTRAP=""
-MAIN_NET=""
-PRIV_NET=""
-COZ_NET=""
+BOOTSTRAP=false
 
-echo "$1"
+if [[ "${@#-c}" != "$@" ]] && [[ "${@#*-m*}" != "$@" ]]; then
+    echo "Error: can not use config file on main net" >&2
+    exit 126
+fi
+
+if [[ "${@#--bootstrap}" != "$@" ]] && [[ "${@#*-p*}" != "$@" ]]; then
+    echo "Error: can not bootstrap on private net" >&2
+    exit 126
+elif [[ "${@#--bootstrap}" != "$@" ]] && [[ "${@#--coznet}" != "$@" ]]; then
+    echo "Error: can not bootstrap on coz net" >&2
+    exit 126
+fi
+
+if [[ "${@#--bootstrap}" = "$@" ]] && [[ "${@#*-n*}" != "$@" ]]; then
+    echo "Error: -n / --notifications option can only be used in case --bootstrap is used as well" >&2
+    exit 126
+fi
 
 while [ -n "$1" ]; do
     case "$1" in
         -m|--mainnet)
             PROMPT_OPTS+="-m"
             BOOTSTRAP_OPTS+="-m"
-            MAIN_NET="-m"
         ;;
         -p|--privnet)
             PROMPT_OPTS+="-p"
-            PRIV_NET="-p"
         ;;
         --coznet)
             PROMPT_OPTS+="--coznet"
-            COZ_NET="--coznet"
         ;;
         -c|--config)
-            if [[ "$MAIN_NET" == "-m" ]]; then
-                echo "Can not use config file on main net, proceeding without config file.."
-            else
-                shift
-                PROMPT_OPTS+="-c=$1"
-                BOOTSTRAP_OPTS+="-c=$1"
-            fi
+            shift
+            PROMPT_OPTS+="-c=$1"
+            BOOTSTRAP_OPTS+="-c=$1"
         ;;
         -t|--set-default-theme)
             shift
@@ -41,27 +47,17 @@ while [ -n "$1" ]; do
             PROMPT_OPTS+="--version=$1"
         ;;
         --bootstrap)
-            if [[ "$PRIV_NET" == "-p" ]]; then
-                echo "Can not bootstrap on private net, proceeding without bootstrapping.."
-            elif [[ "$COZ_NET" == "--coznet" ]]; then
-                echo "Can not bootstrap on coz net, proceeding without bootstrapping.."
-            else
-                BOOTSTRAP="$1"
-            fi
+            BOOTSTRAP=true
         ;;
         -n|--notifications)
-            if [[ -z "$BOOTSTRAP" ]]; then
-                echo "-n / --notifications option can only be used when --bootstrap is used."
-            else
-                BOOTSTRAP_OPTS+="-n"
-            fi
+            BOOTSTRAP_OPTS+="-n"
         ;;
         esac
         shift
 done
 
 rm -rf /neo-python/Chains/privatenet
-if [ ! -d "/neo-python/Chains/SC234" ] && [ ! -z "$BOOTSTRAP" ]; then
+if [ ! -d "/neo-python/Chains/SC234" ] && [[ "$BOOTSTRAP" = true ]]; then
   python3 /neo-python/bootstrap.py --skipconfirm "${BOOTSTRAP_OPTS[@]}"
 fi
 
