026dd2f951e1e551767a793e92ee7b27dded032a
Thomas Saunders <tasaunders@gmail.com>
2018-03-24 23:29:17 -0500

    Bump version: 0.6.4 → 0.6.5-dev

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 setup(
     name='neo-python',
     python_requires='>=3.6',
-    version='0.6.4',
+    version='0.6.5-dev',
     description="Python Node and SDK for the NEO blockchain",
     long_description=readme,
     author="Thomas Saunders",
