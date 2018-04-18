a24294e8f8e0d32688fce5696a966fa0196553c9
Conor Scott <conor.r.scott.88@gmail.com>
2018-04-18 10:38:22 +0400

    Support pip version 10.0 and greater

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
+try:  # pip version >= 10.0
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError:  # pip version < 10.0
+    from pip.req import parse_requirements
+    from pip.download import PipSession
 
 
 with open('README.rst') as readme_file:
