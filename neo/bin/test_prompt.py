e6fcb1e2a1ff53ab2709c24708d4d2997de6a201
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-08-15 23:03:24 -0700

    Update test_prompt.py
    This should work!

--- neo/bin/test_prompt.py
+++ neo/bin/test_prompt.py
@@ -7,31 +7,21 @@ class PromptTest(TestCase):
     def test_prompt_run(self):
 
         child = pexpect.spawn('np-prompt')
-        index = child.expect(["neo", pexpect.EOF, pexpect.TIMEOUT])
-
-        if index == 0:
-            print('np-prompt running as expected')
-        elif index == 1:
-            print('np-prompt experienced an unexpected EOF')
-        elif index == 2:
-            print('np-prompt experienced an unexpected TIMEOUT')
-        elif index != 0 and index != 1 and index != 2:
-            print('test failed')
-
-        self.assertEqual(index, 0)
+        child.expect([pexpect.EOF, pexpect.TIMEOUT])
+        before = child.before
+        text = before.decode('utf-8', 'ignore')
+        checktext = "neo>"
+        self.assertIn(checktext, text)
         child.terminate()
 
     def test_prompt_open_wallet(self):
 
         child = pexpect.spawn('np-prompt')
-        child.sendline('open wallet fixtures/testwallet.db3')
-        child.sendline('testpassword')
-        index = child.expect(['Opened wallet at fixtures/testwallet.db3'])
-
-        if index == 0:
-            print('Opened testwallet.db3 successfully')
-        elif index != 0:
-            print('test failed')
-
-        self.assertEqual(index, 0)
+        child.send('open wallet fixtures/testwallet.db3 \n')
+        child.send('testpassword\n')
+        child.expect([pexpect.EOF, pexpect.TIMEOUT])
+        before = child.before
+        text = before.decode('utf-8', 'ignore')
+        checktext = "Opened"
+        self.assertIn(checktext, text)
         child.terminate()
