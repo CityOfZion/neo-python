d2b3c0d58239d15dfe161e9c92222e987ce1af6d
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-09-21 17:35:28 -0700

    Add args for extended-rpc

--- neo/bin/api_server.py
+++ neo/bin/api_server.py
@@ -126,6 +126,9 @@ def main():
 
     # host
     parser.add_argument("--host", action="store", type=str, help="Hostname ( for example 127.0.0.1)", default="0.0.0.0")
+    
+    # extended json-rpc api
+    parser.add_argument("--extended-rpc", action="store_true", default=False, help="Use extended json-rpc api")
 
     # Now parse
     args = parser.parse_args()
@@ -245,9 +248,17 @@ def main():
     d.setDaemon(True)  # daemonizing the thread will kill it when the main thread is quit
     d.start()
 
-    if args.port_rpc:
+    if args.port_rpc and args.extended_rpc:
+        logger.info("Starting extended json-rpc api server on http://%s:%s" % (args.host, args.port_rpc))
+        api_server_rpc = JsonRpcApi(args.port_rpc, wallet=wallet, extended=True)
+        endpoint_rpc = "tcp:port={0}:interface={1}".format(args.port_rpc, args.host)
+        endpoints.serverFromString(reactor, endpoint_rpc).listen(Site(api_server_rpc.app.resource()))
+#        reactor.listenTCP(int(args.port_rpc), server.Site(api_server_rpc))
+#        api_server_rpc.app.run(args.host, args.port_rpc)
+
+    elif args.port_rpc:
         logger.info("Starting json-rpc api server on http://%s:%s" % (args.host, args.port_rpc))
-        api_server_rpc = JsonRpcApi(args.port_rpc, wallet=wallet)
+        api_server_rpc = JsonRpcApi(args.port_rpc, wallet=wallet, extended=False)
         endpoint_rpc = "tcp:port={0}:interface={1}".format(args.port_rpc, args.host)
         endpoints.serverFromString(reactor, endpoint_rpc).listen(Site(api_server_rpc.app.resource()))
 #        reactor.listenTCP(int(args.port_rpc), server.Site(api_server_rpc))
