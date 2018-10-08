dae906ffb5491c3437c90fb6b3138366d9a24f52
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-10-07 18:57:06 -0700

    Update test_LevelDBBlockchain.py
    for compatibility

--- neo/Implementations/Blockchains/LevelDB/test_LevelDBBlockchain.py
+++ neo/Implementations/Blockchains/LevelDB/test_LevelDBBlockchain.py
@@ -39,25 +39,3 @@ class LevelDBBlockchainTest(BlockchainFixtureTestCase):
         invalid_bc_height = self._blockchain.Height + 1
         block = self._blockchain.GetBlockByHeight(invalid_bc_height)
         self.assertEqual(block, None)
-
-    def test_05_GetHeaderBy(self):
-        # test correct retrieval with hash
-        blockheader = self._blockchain.GetHeaderBy("2b1c78633dae7ab81f64362e0828153079a17b018d779d0406491f84c27b086f")
-        self.assertEqual(blockheader.GetHashCode().ToString(), self._blockchain.GetBlockHash(11).decode('utf-8'))
-
-        # test correct retrieval with 0x hash
-        blockheader = self._blockchain.GetHeaderBy("0x2b1c78633dae7ab81f64362e0828153079a17b018d779d0406491f84c27b086f")
-        self.assertEqual(blockheader.GetHashCode().ToString(), self._blockchain.GetBlockHash(11).decode('utf-8'))
-
-        # test correct retrieval with str height
-        blockheader = self._blockchain.GetHeaderBy("11")
-        self.assertEqual(blockheader.GetHashCode().ToString(), self._blockchain.GetBlockHash(11).decode('utf-8'))
-
-        # test correct retrieval with int height
-        blockheader = self._blockchain.GetHeaderBy(11)
-        self.assertEqual(blockheader.GetHashCode().ToString(), self._blockchain.GetBlockHash(11).decode('utf-8'))
-
-        # test incorrect retrieval
-        invalid_bc_height = self._blockchain.Height + 1
-        block = self._blockchain.GetHeaderBy(invalid_bc_height)
-        self.assertEqual(block, None)
