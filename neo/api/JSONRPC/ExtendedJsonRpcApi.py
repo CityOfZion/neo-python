be45f3b38bd205ba26c6de80d2f49607603d748f
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-10-14 09:50:28 -0700

    Update ExtendedJsonRpcApi.py
    for compatibility

--- neo/api/JSONRPC/ExtendedJsonRpcApi.py
+++ neo/api/JSONRPC/ExtendedJsonRpcApi.py
@@ -60,4 +60,4 @@ class ExtendedJsonRpcApi(JsonRpcApi):
             else:
                 raise JsonRpcError(-400, "Access denied.")
 
-        return super(ExtendedJsonRpcApi, self).json_rpc_method_handler(method, params)
+        return super(ExtendedJsonRpcApi, self).json_rpc_method_handler(method, params) 
