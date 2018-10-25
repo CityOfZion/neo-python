5951b9d454803662a8553b432ced8ba58e127bb3
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-10-25 02:21:26 -0700

    Update test_extended_json_rpc_api.py
    - add tests for `transfertokens`

--- neo/api/JSONRPC/test_extended_json_rpc_api.py
+++ neo/api/JSONRPC/test_extended_json_rpc_api.py
@@ -23,6 +23,10 @@ def mock_request(body):
 class ExtendedJsonRpcApiTestCase(BlockchainFixtureTestCase):
     app = None  # type:JsonRpcApi
 
+    wallet_1_addr = 'AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3'
+
+    watch_addr_str = 'AGYaEi3W6ndHPUmW7T12FFfsbQ6DWymkEm'
+
     @classmethod
     def leveldb_testpath(self):
         return os.path.join(settings.DATA_DIR_PATH, 'fixtures/test_chain')
@@ -115,6 +119,145 @@ class ExtendedJsonRpcApiTestCase(BlockchainFixtureTestCase):
             self.assertIn('blocktime', tx.keys())
             self.assertIsNotNone(tx['blocktime'])
         self.assertEqual(len(res['result']), 9)
+        self.app.wallet.Close()
+        self.app.wallet = None
+        os.remove(WalletFixtureTestCase.wallet_1_dest())
+
+    def test_transfer_tokens_good(self):
+        test_wallet_path = shutil.copyfile(
+            WalletFixtureTestCase.wallet_1_path(),
+            WalletFixtureTestCase.wallet_1_dest()
+        )
+        self.app.wallet = UserWallet.Open(
+            test_wallet_path,
+            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
+        )
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", self.wallet_1_addr, self.watch_addr_str, 1])
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        self.assertEqual(res['result']['type'], "InvocationTransaction")
+        self.assertEqual(res['result']['vout'][0]['address'], self.wallet_1_addr)
+        self.assertEqual(res['result']['net_fee'], "0.0001")
+
+        self.app.wallet.Close()
+        self.app.wallet = None
+        os.remove(WalletFixtureTestCase.wallet_1_dest())
+
+    def test_transfer_tokens_no_wallet(self):
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", self.wallet_1_addr, self.watch_addr_str, 1])
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        error = res.get('error', {})
+        self.assertEqual(error.get('code', None), -400)
+        self.assertEqual(error.get('message', None), "Access denied.")
+
+    def test_transfer_tokens_bad_args(self):  # too few args
+        test_wallet_path = shutil.copyfile(
+            WalletFixtureTestCase.wallet_1_path(),
+            WalletFixtureTestCase.wallet_1_dest()
+        )
+        self.app.wallet = UserWallet.Open(
+            test_wallet_path,
+            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
+        )
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", self.wallet_1_addr, self.watch_addr_str])
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        error = res.get('error', {})
+        self.assertEqual(error.get('code', None), -32602)
+        self.assertEqual(error.get('message', None), "Invalid params")
+
+        self.app.wallet.Close()
+        self.app.wallet = None
+        os.remove(WalletFixtureTestCase.wallet_1_dest())
+
+    def test_transfer_tokens_bad_from_addr(self):
+        test_wallet_path = shutil.copyfile(
+            WalletFixtureTestCase.wallet_1_path(),
+            WalletFixtureTestCase.wallet_1_dest()
+        )
+        self.app.wallet = UserWallet.Open(
+            test_wallet_path,
+            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
+        )
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", "AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc", self.watch_addr_str, 1])  # address is too short
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        error = res.get('error', {})
+        self.assertEqual(error.get('code', None), -32603)
+        self.assertEqual(error.get('message', None), "Not correct Address, wrong length.")
+
+        self.app.wallet.Close()
+        self.app.wallet = None
+        os.remove(WalletFixtureTestCase.wallet_1_dest())
+
+    def test_transfer_tokens_bad_to_addr(self):
+        test_wallet_path = shutil.copyfile(
+            WalletFixtureTestCase.wallet_1_path(),
+            WalletFixtureTestCase.wallet_1_dest()
+        )
+        self.app.wallet = UserWallet.Open(
+            test_wallet_path,
+            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
+        )
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", self.wallet_1_addr, "AGYaEi3W6ndHPUmW7T12FFfsbQ6DWymkE", 1])  # address is too short
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        error = res.get('error', {})
+        self.assertEqual(error.get('code', None), -32602)
+        self.assertEqual(error.get('message', None), "Invalid params")
+
+        self.app.wallet.Close()
+        self.app.wallet = None
+        os.remove(WalletFixtureTestCase.wallet_1_dest())
+
+    def test_transfer_tokens_insufficient_funds(self):
+        test_wallet_path = shutil.copyfile(
+            WalletFixtureTestCase.wallet_1_path(),
+            WalletFixtureTestCase.wallet_1_dest()
+        )
+        self.app.wallet = UserWallet.Open(
+            test_wallet_path,
+            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
+        )
+
+        token = list(self.app.wallet.GetTokens().values())[0]
+        balance = self.app.wallet.GetBalance(token)
+
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", self.wallet_1_addr, self.watch_addr_str, int(balance) + 1])
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        error = res.get('error', {})
+        self.assertEqual(error.get('code', None), -32602)
+        self.assertEqual(error.get('message', None), "Invalid params")
+
+        self.app.wallet.Close()
+        self.app.wallet = None
+        os.remove(WalletFixtureTestCase.wallet_1_dest())
+
+    def test_transfer_tokens_weird_amount(self):
+        test_wallet_path = shutil.copyfile(
+            WalletFixtureTestCase.wallet_1_path(),
+            WalletFixtureTestCase.wallet_1_dest()
+        )
+        self.app.wallet = UserWallet.Open(
+            test_wallet_path,
+            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
+        )
+        req = self._gen_rpc_req("transfertokens", params=["NXT4", self.wallet_1_addr, self.watch_addr_str, "blah"])
+        mock_req = mock_request(json.dumps(req).encode("utf-8"))
+        res = json.loads(self.app.home(mock_req))
+
+        error = res.get('error', {})
+        self.assertEqual(error.get('code', None), -32603)
+        self.assertEqual(error.get('message', None), "[<class 'decimal.ConversionSyntax'>]")
+
         self.app.wallet.Close()
         self.app.wallet = None
         os.remove(WalletFixtureTestCase.wallet_1_dest())
