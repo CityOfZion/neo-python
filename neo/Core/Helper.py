48eedfddb6767f779566ebde82c4601cdff8da92
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-11-13 13:14:12 -0800

    Update Helper.py
    - add `StaticAssetState` to Helper

--- neo/Core/Helper.py
+++ neo/Core/Helper.py
@@ -226,3 +226,22 @@ class Helper:
     def EmitServiceEvents(state_reader):
         for event in state_reader.events_to_dispatch:
             events.emit(event.event_type, event)
+
+    @staticmethod
+    def StaticAssetState(assetId):
+        neo = AssetState()
+        neo.AssetId = UInt256.ParseString("c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b")
+        neo.AssetType = 0
+
+        gas = AssetState()
+        gas.AssetId = UInt256.ParseString("602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7")
+        gas.AssetType = 1
+
+        if assetId == neo.AssetId:
+            return neo
+
+        elif assetId == gas.AssetId:
+            return gas
+
+        else:
+            return None
