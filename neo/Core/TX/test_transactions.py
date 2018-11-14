49dff15fdeaaa7d364d06a081e19df64eaeed4f0
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-11-13 17:28:06 -0800

    Update test_transactions.py
    - revert to previous passing build

--- neo/Core/TX/test_transactions.py
+++ neo/Core/TX/test_transactions.py
@@ -1,4 +1,4 @@
-from neo.Utils.BlockchainFixtureTestCase import BlockchainFixtureTestCase
+from neo.Utils.NeoTestCase import NeoTestCase
 from neo.Core.TX.MinerTransaction import MinerTransaction
 from neo.Core.TX.Transaction import Transaction, TransactionType
 from neocore.IO.BinaryWriter import BinaryWriter
@@ -8,19 +8,10 @@ from neo.IO.MemoryStream import MemoryStream, StreamManager
 import binascii
 import os
 from neo.Settings import settings
-import shutil
-from neo.Implementations.Wallets.peewee.UserWallet import UserWallet
-from neo.Wallets.utils import to_aes_key
-from neo.Utils.WalletFixtureTestCase import WalletFixtureTestCase
-from examples.build_raw_transactions import example1, example2
 from mock import patch
 
 
-class TransactionTestCase(BlockchainFixtureTestCase):
-
-    @classmethod
-    def leveldb_testpath(self):
-        return os.path.join(settings.DATA_DIR_PATH, 'fixtures/test_chain')
+class TransactionTestCase(NeoTestCase):
 
     def test_tx_types(self):
         self.assertEqual('ContractTransaction', TransactionType.ToName(TransactionType.ContractTransaction))
@@ -226,31 +217,3 @@ class TransactionTestCase(BlockchainFixtureTestCase):
                     tx.GetScriptHashesForVerifying()
 
         self.assertTrue("Invalid operation" in str(e.exception))
-
-    def test_build_raw_transactions(self):
-        test_wallet_path = shutil.copyfile(
-            WalletFixtureTestCase.wallet_1_path(),
-            WalletFixtureTestCase.wallet_1_dest()
-        )
-        wallet = UserWallet.Open(
-            test_wallet_path,
-            to_aes_key(WalletFixtureTestCase.wallet_1_pass())
-        )
-        # test example1
-        with patch("neo.Implementations.Wallets.peewee.UserWallet.UserWallet.Create", return_value=wallet):
-            with patch("neo.Implementations.Wallets.peewee.UserWallet.UserWallet.CreateKey"):
-                res = example1()
-
-                self.assertTrue(res)
-                self.assertEqual(res.decode('utf-8'), "80000190274d792072617720636f6e7472616374207472616e73616374696f6e206465736372697074696f6e01949354ea0a8b57dfee1e257a1aedd1e0eea2e5837de145e8da9c0f101bfccc8e0100029b7cffdaa674beae0f930ebe6085af9093e5fe56b34a5c220ccdcf6efc336fc500a3e11100000000ea610aa6db39bd8c8556c9569d94b5e5a5d0ad199b7cffdaa674beae0f930ebe6085af9093e5fe56b34a5c220ccdcf6efc336fc5004f2418010000001cc9c05cefffe6cdd7b182816a9152ec218d2ec000")
-
-        # test example2
-        with patch("neo.Implementations.Wallets.peewee.UserWallet.UserWallet.Create", return_value=wallet):
-            with patch("neo.Implementations.Wallets.peewee.UserWallet.UserWallet.CreateKey"):
-                res = example2()
-
-                self.assertTrue(res)
-                self.assertEqual(res.decode('utf-8'), "d1001b00046e616d6567d3d8602814a429a91afdbaa3914884a1c90c73310290274d792072617720636f6e7472616374207472616e73616374696f6e206465736372697074696f6e201cc9c05cefffe6cdd7b182816a9152ec218d2ec000000141405bd8ba993473b51bfa338dd3f2d4a236b4e940572cf6f077c411cd3a5fa8ccce09d945159a3978e7697915620473da0e2189048d768ed2a70535a73a9cba3a33232103cbb45da6072c14761c9da545749d9cfd863f860c351066d16df480602a2024c6ac")
-
-        wallet.Close()
-        os.remove(WalletFixtureTestCase.wallet_1_dest())
