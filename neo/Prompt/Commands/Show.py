2c2fce76c1ad3e6cb02bda5f30f852871a75bd91
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-12-05 06:56:04 -0800

    Create Show.py
    Adds Command Show and supports `show block` through `show nodes`

---
+++ neo/Prompt/Commands/Show.py
@@ -0,0 +1,167 @@
+import os
+import psutil
+import datetime
+from neo.Prompt.CommandBase import CommandBase, CommandDesc, ParameterDesc
+from neo.Prompt.PromptData import PromptData
+from neo.Prompt.Utils import get_arg
+from neo.Core.Blockchain import Blockchain
+from prompt_toolkit.shortcuts import print_formatted_text
+from prompt_toolkit.formatted_text import FormattedText
+from neocore.UInt256 import UInt256
+from neo.IO.MemoryStream import StreamManager
+from neo.Network.NodeLeader import NodeLeader
+from neo.Implementations.Notifications.LevelDB.NotificationDB import NotificationDB
+from neo.logging import log_manager
+import json
+
+
+logger = log_manager.getLogger()
+
+
+class CommandShow(CommandBase):
+    def __init__(self):
+        super().__init__()
+
+        self.register_sub_command(CommandShowBlock())
+        self.register_sub_command(CommandShowHeader())
+        self.register_sub_command(CommandShowTx())
+        self.register_sub_command(CommandShowMem())
+        self.register_sub_command(CommandShowNodes(), ['node'])
+
+    def command_desc(self):
+        return CommandDesc('show', 'show data from the blockchain')
+
+    def execute(self, arguments):
+        item = get_arg(arguments)
+
+        if not item:
+            print("run `show help` to see supported queries")
+            return
+
+        try:
+            return self.execute_sub_command(item, arguments[1:])
+        except KeyError:
+            print(f"show: {item} is an invalid parameter")
+            return
+
+
+class CommandShowBlock(CommandBase):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, arguments):
+        item = get_arg(arguments)
+        txarg = get_arg(arguments, 1)
+
+        block = Blockchain.Default().GetBlock(item)
+
+        if block is not None:
+            block.LoadTransactions()
+            bjson = json.dumps(block.ToJson(), indent=4)
+            if txarg and 'tx' in txarg:
+                txs = []
+                for tx in block.FullTransactions:
+                    tjson = json.dumps(tx.ToJson(), indent=4)
+                    tokens = [("class:number", tjson)]
+                    print_formatted_text(FormattedText(tokens), style=PromptData.Prompt.token_style)
+                    txs.append(tx.ToJson())
+                return txs
+
+            tokens = [("class:number", bjson)] 
+            print_formatted_text(FormattedText(tokens), style=PromptData.Prompt.token_style)
+            return block.ToJson()
+
+        else:
+            print("Could not locate block %s" % item)
+            return
+
+    def command_desc(self):
+        p1 = ParameterDesc('index/hash', 'the index or scripthash of the block')
+        p2 = ParameterDesc('tx', 'arg to only show block transactions', optional=True)
+        return CommandDesc('block', 'show a specified block', [p1, p2])
+
+
+class CommandShowHeader(CommandBase):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, arguments):
+        item = get_arg(arguments)
+
+        header = Blockchain.Default().GetHeaderBy(item)
+        if header is not None:
+            hjson = (json.dumps(header.ToJson(), indent=4))
+            tokens = [("class:number", hjson)]
+            print_formatted_text(FormattedText(tokens), style=PromptData.Prompt.token_style)
+            return header.ToJson()
+        else:
+            print("Could not locate header %s\n" % item)
+            return
+
+    def command_desc(self):
+        p1 = ParameterDesc('index/hash', 'the index or scripthash of the block header')
+        return CommandDesc('header', 'show the header of a specified block', [p1])
+
+
+class CommandShowTx(CommandBase):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, arguments):
+        try:
+            txid = UInt256.ParseString(get_arg(arguments))
+            tx, height = Blockchain.Default().GetTransaction(txid)
+            if height > -1:
+                jsn = tx.ToJson()
+                jsn['height'] = height
+                jsn['unspents'] = [uns.ToJson(tx.outputs.index(uns)) for uns in
+                                   Blockchain.Default().GetAllUnspent(txid)]
+                tokens = [("class:command", json.dumps(jsn, indent=4))]
+                print_formatted_text(FormattedText(tokens), style=PromptData.Prompt.token_style)
+                return jsn
+            else:
+                print(f"Could not find transaction for hash {txid}")
+                return
+        except Exception as e:
+            print("Could not find transaction from args: %s (%s)" % (e, arguments))
+            return
+
+    def command_desc(self):
+        p1 = ParameterDesc('hash', 'the scripthash of the transaction')
+        return CommandDesc('tx', 'show a specified transaction', [p1])
+
+
+class CommandShowMem(CommandBase):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, arguments=None):
+        process = psutil.Process(os.getpid())
+        total = process.memory_info().rss
+        totalmb = total / (1024 * 1024)
+        out = "Total: %s MB\n" % totalmb
+        out += "Total buffers: %s\n" % StreamManager.TotalBuffers()
+        print_formatted_text(FormattedText([("class:number", out)]), style=PromptData.Prompt.token_style)
+        return out
+
+    def command_desc(self):
+        return CommandDesc('mem', 'show memory in use and number of buffers')
+
+
+class CommandShowNodes(CommandBase):
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, arguments=None):
+        if len(NodeLeader.Instance().Peers) > 0:
+            out = "Total Connected: %s\n" % len(NodeLeader.Instance().Peers)
+            for peer in NodeLeader.Instance().Peers:
+                out += "Peer %s - IO: %s\n" % (peer.Name(), peer.IOStats())
+            print_formatted_text(FormattedText([("class:number", out)]), style=PromptData.Prompt.token_style)
+            return out
+        else:
+            print("Not connected yet\n")
+            return
+
+    def command_desc(self):
+        return CommandDesc('nodes', 'show connected peers')
