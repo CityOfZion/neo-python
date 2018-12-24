a21973cab1472e02768c2fa97e69693fcf178be0
jseagrave21 <40873301+jseagrave21@users.noreply.github.com>
2018-12-23 22:53:52 -0700

    Update Settings.py
    - fix indentation

--- neo/Settings.py
+++ neo/Settings.py
@@ -260,11 +260,11 @@ class SettingsHolder:
             os.makedirs(self.DATA_DIR_PATH)
 
      def set_max_peers(self, num_peers):
-        maxpeers = int(num_peers)
-        if maxpeers > 0:
-            self.CONNECTED_PEER_MAX = maxpeers
-        else:
-            raise ValueError
+         maxpeers = int(num_peers)
+         if maxpeers > 0:
+             self.CONNECTED_PEER_MAX = maxpeers
+         else:
+             raise ValueError
 
     def set_log_smart_contract_events(self, is_enabled=True):
         self.log_smart_contract_events = is_enabled
