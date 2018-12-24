b8078c770f1edaadc57732658f65cdcecf8874fc
Guillaume George <lysandergc@gmail.com>
2018-12-24 18:19:22 +0900

    test_wallet_commands.py: Move tests of non commands at the end
    Signed-off-by: Guillaume George <lysandergc@gmail.com>

--- neo/Prompt/Commands/tests/test_wallet_commands.py
+++ neo/Prompt/Commands/tests/test_wallet_commands.py
@@ -467,101 +467,6 @@ class UserWalletTestCase(WalletFixtureTestCase):
         self.assertTrue(res)
         self.assertIn('mine', [n.Title for n in PromptData.Wallet.NamedAddr])
 
-    ##########################################################
-    ##########################################################
-
-    def test_3_import_token(self):
-        wallet = self.GetWallet1(recreate=True)
-
-        self.assertEqual(len(wallet.GetTokens()), 1)
-
-        token_hash = '31730cc9a1844891a3bafd1aa929a4142860d8d3'
-
-        ImportToken(wallet, token_hash)
-
-        token = list(wallet.GetTokens().values())[0]
-
-        self.assertEqual(token.name, 'NEX Template V4')
-        self.assertEqual(token.symbol, 'NXT4')
-        self.assertEqual(token.decimals, 8)
-        self.assertEqual(token.Address, 'Ab61S1rk2VtCVd3NtGNphmBckWk4cfBdmB')
-
-    def test_4_get_synced_balances(self):
-        wallet = self.GetWallet1(recreate=True)
-        synced_balances = wallet.GetSyncedBalances()
-        self.assertEqual(len(synced_balances), 2)
-
-    def test_5_show_unspent(self):
-        wallet = self.GetWallet1(recreate=True)
-        unspents = ShowUnspentCoins(wallet)
-        self.assertEqual(len(unspents), 2)
-
-        unspents = ShowUnspentCoins(wallet, asset_id=self.NEO)
-        self.assertEqual(len(unspents), 1)
-
-        unspents = ShowUnspentCoins(wallet, asset_id=self.GAS)
-        self.assertEqual(len(unspents), 1)
-
-        unspents = ShowUnspentCoins(wallet, from_addr=wallet.ToScriptHash('AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3'))
-        self.assertEqual(len(unspents), 2)
-
-        unspents = ShowUnspentCoins(wallet, from_addr=wallet.ToScriptHash('AYhE3Svuqdfh1RtzvE8hUhNR7HSpaSDFQg'))
-        self.assertEqual(len(unspents), 0)
-
-        unspents = ShowUnspentCoins(wallet, watch_only=True)
-        self.assertEqual(len(unspents), 0)
-
-    def test_6_split_unspent(self):
-        wallet = self.GetWallet1(recreate=True)
-
-        # test bad
-        tx = SplitUnspentCoin(wallet, [])
-        self.assertEqual(tx, None)
-
-        # bad inputs
-        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'neo', 3, 2])
-        self.assertEqual(tx, None)
-
-        # should be ok
-        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'neo', 0, 2], prompt_passwd=False)
-        self.assertIsNotNone(tx)
-
-        # rebuild wallet and try with non-even amount of neo, should be split into integer values of NEO
-        wallet = self.GetWallet1(True)
-        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'neo', 0, 3], prompt_passwd=False)
-        self.assertIsNotNone(tx)
-
-        self.assertEqual([Fixed8.FromDecimal(17), Fixed8.FromDecimal(17), Fixed8.FromDecimal(16)], [item.Value for item in tx.outputs])
-
-        # try with gas
-        wallet = self.GetWallet1(True)
-        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'gas', 0, 3], prompt_passwd=False)
-        self.assertIsNotNone(tx)
-
-    def test_7_create_address(self):
-        # no wallet
-        res = CreateAddress(None, 1)
-        self.assertFalse(res)
-
-        wallet = self.GetWallet1(recreate=True)
-
-        # not specifying a number of addresses
-        res = CreateAddress(wallet, None)
-        self.assertFalse(res)
-
-        # bad args
-        res = CreateAddress(wallet, "blah")
-        self.assertFalse(res)
-
-        # negative value
-        res = CreateAddress(wallet, -1)
-        self.assertFalse(res)
-
-        # should pass
-        res = CreateAddress(wallet, 2)
-        self.assertTrue(res)
-        self.assertEqual(len(wallet.Addresses), 3)
-
     def test_wallet_export_baseclass(self):
         self.OpenWallet1()
 
@@ -875,3 +780,98 @@ class UserWalletTestCase(WalletFixtureTestCase):
             res = CommandWallet().execute(args)
             self.assertTrue(res)
             self.assertIn("Added multi-sig contract address", mock_print.getvalue())
+
+    ##########################################################
+    ##########################################################
+
+    def test_3_import_token(self):
+        wallet = self.GetWallet1(recreate=True)
+
+        self.assertEqual(len(wallet.GetTokens()), 1)
+
+        token_hash = '31730cc9a1844891a3bafd1aa929a4142860d8d3'
+
+        ImportToken(wallet, token_hash)
+
+        token = list(wallet.GetTokens().values())[0]
+
+        self.assertEqual(token.name, 'NEX Template V4')
+        self.assertEqual(token.symbol, 'NXT4')
+        self.assertEqual(token.decimals, 8)
+        self.assertEqual(token.Address, 'Ab61S1rk2VtCVd3NtGNphmBckWk4cfBdmB')
+
+    def test_4_get_synced_balances(self):
+        wallet = self.GetWallet1(recreate=True)
+        synced_balances = wallet.GetSyncedBalances()
+        self.assertEqual(len(synced_balances), 2)
+
+    def test_5_show_unspent(self):
+        wallet = self.GetWallet1(recreate=True)
+        unspents = ShowUnspentCoins(wallet)
+        self.assertEqual(len(unspents), 2)
+
+        unspents = ShowUnspentCoins(wallet, asset_id=self.NEO)
+        self.assertEqual(len(unspents), 1)
+
+        unspents = ShowUnspentCoins(wallet, asset_id=self.GAS)
+        self.assertEqual(len(unspents), 1)
+
+        unspents = ShowUnspentCoins(wallet, from_addr=wallet.ToScriptHash('AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3'))
+        self.assertEqual(len(unspents), 2)
+
+        unspents = ShowUnspentCoins(wallet, from_addr=wallet.ToScriptHash('AYhE3Svuqdfh1RtzvE8hUhNR7HSpaSDFQg'))
+        self.assertEqual(len(unspents), 0)
+
+        unspents = ShowUnspentCoins(wallet, watch_only=True)
+        self.assertEqual(len(unspents), 0)
+
+    def test_6_split_unspent(self):
+        wallet = self.GetWallet1(recreate=True)
+
+        # test bad
+        tx = SplitUnspentCoin(wallet, [])
+        self.assertEqual(tx, None)
+
+        # bad inputs
+        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'neo', 3, 2])
+        self.assertEqual(tx, None)
+
+        # should be ok
+        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'neo', 0, 2], prompt_passwd=False)
+        self.assertIsNotNone(tx)
+
+        # rebuild wallet and try with non-even amount of neo, should be split into integer values of NEO
+        wallet = self.GetWallet1(True)
+        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'neo', 0, 3], prompt_passwd=False)
+        self.assertIsNotNone(tx)
+
+        self.assertEqual([Fixed8.FromDecimal(17), Fixed8.FromDecimal(17), Fixed8.FromDecimal(16)], [item.Value for item in tx.outputs])
+
+        # try with gas
+        wallet = self.GetWallet1(True)
+        tx = SplitUnspentCoin(wallet, ['AJQ6FoaSXDFzA6wLnyZ1nFN7SGSN2oNTc3', 'gas', 0, 3], prompt_passwd=False)
+        self.assertIsNotNone(tx)
+
+    def test_7_create_address(self):
+        # no wallet
+        res = CreateAddress(None, 1)
+        self.assertFalse(res)
+
+        wallet = self.GetWallet1(recreate=True)
+
+        # not specifying a number of addresses
+        res = CreateAddress(wallet, None)
+        self.assertFalse(res)
+
+        # bad args
+        res = CreateAddress(wallet, "blah")
+        self.assertFalse(res)
+
+        # negative value
+        res = CreateAddress(wallet, -1)
+        self.assertFalse(res)
+
+        # should pass
+        res = CreateAddress(wallet, 2)
+        self.assertTrue(res)
+        self.assertEqual(len(wallet.Addresses), 3)
