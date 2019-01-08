403ce4ba7d0e4bbf2a7bd04f9ee39b6d9268156e
Erik van den Brink <git@erikvandenbrink.com>
2019-01-09 00:11:17 +0100

    test print fix

--- neo/Prompt/PromptPrinter.py
+++ neo/Prompt/PromptPrinter.py
@@ -2,6 +2,7 @@ from prompt_toolkit import print_formatted_text
 from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.styles import Style
 from neo.UserPreferences import preferences
+import os
 
 token_style = Style.from_dict({
     "command": preferences.token_style['Command'],
@@ -34,6 +35,9 @@ class PromptPrinter():
 
 
 pp = PromptPrinter()
+
+if 'NEOPYTHON_UNITTEST' in os.environ:
+    pp = print
 
 
 def prompt_print(*args, **kwargs):
