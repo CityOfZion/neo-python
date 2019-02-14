fe90f62e123d720d4281c79af0598d9df9e776fb
Erik van den Brink <git@erikvandenbrink.com>
2019-02-14 14:28:39 +0100

    Bump version: 0.8.4-dev → 0.8.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 setup(
     name='neo-python',
     python_requires='>=3.6',
-    version='0.8.4-dev',
+    version='0.8.4',
     description="Python Node and SDK for the NEO blockchain",
     long_description=readme,
     author="Thomas Saunders",
